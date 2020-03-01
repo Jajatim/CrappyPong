20096448f7e08803a7cb1a2190634c64d7cc098c
Scarlatine <>
2020-03-01 15:49:45 +0100

    placeholder for render

--- src/render.c
+++ src/render.c
@@ -11,5 +11,12 @@ void Render(Game *pGame) {
 }
 
 void RenderGame(Game *pGame) {
+    SDL_Rect dst;
     
+    dst.x = 0;
+    dst.y = 0;
+    dst.w = 10;
+    dst.h = 50;
+    SDL_SetRenderDrawColor(pGame->pRenderer,255,255,255,255);
+    SDL_RenderFillRect(pGame->pRenderer, &dst);
 }
