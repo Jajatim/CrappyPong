3622de06f166e9c7267296aa4b5b1809b2944b3f
Scarlatine <>
2020-03-01 16:11:25 +0100

    placeholder i

--- src/update.c
+++ src/update.c
@@ -1,5 +1,32 @@
 #include "update.h"
 
 void Update(Game *pGame, Uint32 deltaTime) {
-    
+    /*
+    Mouse
+        int MouseX;
+        int MouseY;
+        int MouseLeftClic;
+        int MouseRightClic;
+        int MouseWheelUp;
+        int MouseWheelDown;
+        
+    Keyboard
+        int ArrowUp;
+        int ArrowLeft;
+        int ArrowDown;
+        int ArrowRight;
+        int Keyz;
+        int Keyq;
+        int Keys;
+        int Keyd;
+        int KeySpace;
+
+    Example : 
+        pGame->pKeyboard->ArrowUp
+    */
+
+    if (pGame->pKeyboard->ArrowUp) {
+        printf("Arrow up : ON\n");
+        pGame->pKeyboard->ArrowUp = 0;
+    }
 }
